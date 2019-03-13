f6640d9c56cf51ddb863efbe764115c40d0b429f
ywlqjl <yanwenliqjl@gmail.com>
2019-03-13 13:38:28 +0100

    Add 2 points

--- app/src/main/java/com/yanwenli/prd_2/AugmentedImageNode.java
+++ app/src/main/java/com/yanwenli/prd_2/AugmentedImageNode.java
@@ -19,23 +19,36 @@ import java.util.concurrent.CompletableFuture;
 
 public class AugmentedImageNode extends AnchorNode implements Node.OnTapListener, Node.OnTouchListener {
 
-
     private static final String TAG = "AugmentedImageNode"; //节点标签
     private AugmentedImage image; //增强的图像
-    private static CompletableFuture<ViewRenderable> test;
+    private static CompletableFuture<ViewRenderable> point;
+    private static CompletableFuture<ViewRenderable> point_left;
+    private static CompletableFuture<ViewRenderable> point_right;
     private Node cornerNode;
+    private Node cornerNode_left;
+    private Node cornerNode_right;
     public Context nodeContext;
 
     private Anchor anchorParent;
 
 
+    /**
+     * Build renderable images with layout.
+     * @param context
+     */
     @RequiresApi(api = Build.VERSION_CODES.N)
     public AugmentedImageNode(Context context) {
         this.nodeContext = context;
-        if (test == null) {
+        if (point == null) {
 
-            test = ViewRenderable.builder()
+            point = ViewRenderable.builder()
                     .setView(context, R.layout.video_play_layout_renderable)
+                    .build();
+            point_left = ViewRenderable.builder()
+                    .setView(context, R.layout.intrest_point_layout_renderable)
+                    .build();
+            point_right = ViewRenderable.builder()
+                    .setView(context, R.layout.intrest_point_layout_renderable2)
                     .build();
         }
     }
@@ -44,11 +57,18 @@ public class AugmentedImageNode extends AnchorNode implements Node.OnTapListener
         return image;
     }
 
+    /**
+     * Load the ViewRenderable points.
+     * Define their local positions, angle and the world position.
+     * Bind the points with nodes
+     * Connect to the parent node.
+     * @param image
+     */
     public void setImage(AugmentedImage image) {
         this.image = image;
 
-        if (!test.isDone()) {
-            CompletableFuture.allOf(test)
+        if (!point.isDone()||!point_left.isDone()||!point_right.isDone()) {
+            CompletableFuture.allOf(point,point_left,point_right)
                     .thenAccept((Void aVoid) -> setImage(image))
                     .exceptionally(
                             throwable -> {
@@ -61,23 +81,38 @@ public class AugmentedImageNode extends AnchorNode implements Node.OnTapListener
         anchorParent = image.createAnchor(image.getCenterPose());
         setAnchor(anchorParent);
 
+        // Create the local position Vector for every point
         Vector3 localPosition = new Vector3();
+        Vector3 localPosition2 = new Vector3();
+        Vector3 localPosition3 = new Vector3();
 
-//        test position
-//        localPosition.set(-0.0f, 0.0f, 0.0f );
+        // Centre position: localPosition.set(-0.0f, 0.0f, 0.0f );
         localPosition.set(-0.0f * image.getExtentX(), -0.0f, 0.0f * image.getExtentZ());
+        localPosition2.set(-0.5f * image.getExtentX(), -0.0f, 0.7f * image.getExtentZ());
+        localPosition3.set(0.5f * image.getExtentX(), -0.0f, -0.2f * image.getExtentZ());
 
-        /*
-        localPosition.set(0.2f * image.getExtentX(), -0.0f , 0.39f * image.getExtentZ());*/
-        //TODO:Vérifie si ça marche
+        // Create world position with quaternion for changing local position to world position
+        Quaternion quaternion = new Quaternion(0.0f, 0.0f * image.getExtentX(), 0.0f, 0.0f * image.getExtentZ());
+
+        // Create the nodes and set their positions.
         cornerNode = new Node();
         cornerNode.setParent(this);
         cornerNode.setLocalPosition(localPosition);
-        Quaternion quaternion = new Quaternion(0.0f, 0.0f * image.getExtentX(), 0.0f, 0.0f * image.getExtentZ());
 //        cornerNode.setLocalRotation(quaternion);
         cornerNode.setWorldRotation(quaternion);
-        cornerNode.setRenderable(test.getNow(null));
+        cornerNode.setRenderable(point.getNow(null));
 
+        cornerNode_left = new Node();
+        cornerNode_left.setParent(this);
+        cornerNode_left.setLocalPosition(localPosition2);
+        cornerNode_left.setWorldRotation(quaternion);
+        cornerNode_left.setRenderable(point_left.getNow(null));
+
+        cornerNode_right = new Node();
+        cornerNode_right.setParent(this);
+        cornerNode_right.setLocalPosition(localPosition3);
+        cornerNode_right.setWorldRotation(quaternion);
+        cornerNode_right.setRenderable(point_right.getNow(null));
 
     }
 
