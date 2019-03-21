89a586e2eb379ec0531132bd655226ff72fab7e8
ywlqjl <yanwenliqjl@gmail.com>
2019-03-21 10:28:13 +0100

    Add mainActivity Test in both "test" and "instrumentedTest"

---
+++ app/src/androidTest/java/com/yanwenli/prd_2/MainActivityInstrumentTest.java
@@ -0,0 +1,42 @@
+package com.yanwenli.prd_2;
+
+import android.support.test.espresso.contrib.DrawerActions;
+import android.support.test.espresso.contrib.NavigationViewActions;
+import android.support.test.rule.ActivityTestRule;
+import android.support.test.runner.AndroidJUnit4;
+import android.view.Gravity;
+
+import org.junit.Rule;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+
+import static android.support.test.espresso.Espresso.onView;
+import static android.support.test.espresso.assertion.ViewAssertions.matches;
+import static android.support.test.espresso.contrib.DrawerMatchers.isClosed;
+import static android.support.test.espresso.matcher.ViewMatchers.withId;
+
+@RunWith(AndroidJUnit4.class)
+public class MainActivityInstrumentTest {
+
+    @Rule
+    public ActivityTestRule<MainActivity> mActivityRule = new ActivityTestRule<>(MainActivity.class);
+
+    @Test
+    public void onNavigationItemSelected() {
+
+    }
+
+    @Test
+    public void clickOnYourNavigationItem_ShowsYourScreen() {
+        // Open Drawer to click on navigation.
+        onView(withId(R.id.drawer_layout))
+                .check(matches(isClosed(Gravity.LEFT))) // Left Drawer should be closed.
+                .perform(DrawerActions.open()); // Open Drawer
+
+
+        // Start the screen of your activity.
+        onView(withId(R.id.nav_view))
+                .perform(NavigationViewActions.navigateTo(R.id.nav_camera));
+
+    }
+}
